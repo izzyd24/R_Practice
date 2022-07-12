113f3ff07a509ef69e08137adc44a0b9ece7249a
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-07-11 20:23:30 -0700

    15.3.7 facet fixed

--- R_Analysis/01_Demo/15_practice.R
+++ R_Analysis/01_Demo/15_practice.R
@@ -33,3 +33,27 @@ plt + geom_tile() + labs(x="Model",y="Vehicle Year",fill="Mean Highway (MPG)")+
 theme(axis.text.x = element_text(angle=90,hjust=1,vjust=.5))
 
 # 15.3.7 layers to plots
+# import dataset into ggplot
+plt <- ggplot(mpg,aes(x=manufacturer,y=hwy))
+# add boxplot
+plt + geom_boxplot() +
+# rotation 
+theme(axis.text.x=element_text(angle=45,hjust=1)) +
+# overlay scatter plot on top
+geom_point()
+
+# mapping and data arguments added
+# with context on stdev of engine size for each car class
+mpg_summary <- mpg %>% group_by(class) %>% summarize(Mean_Engine=mean(displ),SD_Engine=sd(displ), .groups = 'keep')
+plt <- ggplot(mpg_summary,aes(x=class,y=Mean_Engine))
+plt + geom_point(size=4) + labs(x="Vehicle Class",y="Mean Engine Size") +
+geom_errorbar(aes(ymin=Mean_Engine-SD_Engine,ymax=Mean_Engine+SD_Engine))
+
+# convert to long format
+mpg_long <- mpg %>% gather(key="MPG_Type",value="Rating",c(cty,hwy))
+
+# import dataset into ggplot2
+plt <- ggplot(mpg_long,aes(x=manufacturer,y=Rating,color=MPG_Type))
+plt + geom_boxplot() + facet_wrap(vars(MPG_Type)) +
+# add boxplot with labels, 45 degree rotation
+theme(axis.text.x=element_text(angle=45,hjust=1),legend.position = "none") + xlab("Manufacturer")
