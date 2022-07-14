6dbf245bed57431892440948a78d3ec5c5d3607b
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-07-13 22:12:28 -0700

    job page vs total page views

--- R_Analysis/01_Demo/real_example.R
+++ R_Analysis/01_Demo/real_example.R
@@ -15,8 +15,27 @@ plt + geom_point()
 
 # inital look: strong positive correlation between the two
 # find correlation coeff
-cor(li_visitors_data$Jobs.page.views..total., li_visitors_data$Total.page.views..total.)
+# URGENT: IF NA output, we have missing values
+# need to tell R how to compute these with "use" argument
 
 # cor matrix for multiple columns at once
 li_matrix <- as.matrix(li_visitors_data[c("Total.unique.visitors..total.", "Total.page.views..total.", "Jobs.page.views..total.")])
-cor(li_matrix)
+# can use = complete.obs OR na,omit(li_matrix) as needed
+cor(li_matrix, use="complete.obs")
+
+# create a linear reg for the total job views x, and 
+# total views y
+summary(lm(Jobs.page.views..total.~Total.page.views..total., li_visitors_data))
+
+# shows that .72 r square value; or 72% of variability of dependent
+# ... is explained by linear model
+# .72 = high level of correlation that jobs views affects total views
+# p-value < sig level of 0.05%; sufficient evidence to reject null, ho
+# the slope of model is not zero!
+
+# plot the linear model; overlay seniority as color
+link_plot <- ggplot(li_visitors_data, aes(x=Total.page.views..total., y=Jobs.page.views..total.)) +
+  geom_point(col="steelblue", size=2) +
+  geom_smooth(method="lm", colour="red") +
+  labs(title="Jobs Page Views Impact on Total Page Views", y="Total Page Views", x="Job Page Views")
+plot(link_plot) 
